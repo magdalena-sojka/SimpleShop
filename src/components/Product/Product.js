335e678cb24295c9ccd58a14b21a23f1c613f7a3
Magdalena Sojka <sojka.magdalena.90@gmail.com>
2022-04-06 21:01:30 +0200

    3/6

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,11 +3,16 @@ import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import shortid from 'shortid';
 
 const Product = ({ name, title, colors, sizes, basePrice }) => {
 
   const [currentColor, setCurrentColor] = useState(colors[0]);
   const [currentSize, setCurrentSize] = useState(sizes[0]);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
 
   return (
     <article className={styles.product}>
@@ -26,18 +31,21 @@ const Product = ({ name, title, colors, sizes, basePrice }) => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {sizes.map(size =>
+                <li key={shortid()}>
+                  <button type="button" className={clsx(currentSize === size && styles.active)}>{size.name}</button>
+                </li>
+              )}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map(color =>
+                <li key={shortid()}>
+                  <button type="button" className={clsx(prepareColorClassName(color), currentColor === color && styles.active)}></button>
+                </li>
+              )}
             </ul>
           </div>
           <Button className={styles.button}>
